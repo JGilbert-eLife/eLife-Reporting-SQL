7fd7d6fb55c9cb96744046692c5c6c7c71b4f77a
James Gilbert <j.gilbert@elifesciences.org>
2014-07-02 15:17:58 +0100

    Updated SQL to account for changing information

--- TodaysData.sh
+++ TodaysData.sh
@@ -45,9 +45,10 @@ if [ -e $initialinput ]; then
 egrep "^\"[[:digit:]]*\"," $initialinput > initial_tmp.csv
 tr -d "\"" < initial_tmp.csv > initial.csv
 rm initial_tmp.csv
-echo "delete FROM initial_import;" >> /tmp/sql.cmds #
+echo "DELETE FROM initial_import;" >> /tmp/sql.cmds #
 echo ".import initial.csv initial_import" >> /tmp/sql.cmds #
-echo "insert INTO initial SELECT * FROM initial_import WHERE initial_import.ms NOT IN (SELECT ms FROM initial);" >> /tmp/sql.cmds #
+echo "INSERT INTO initial SELECT * FROM initial_import WHERE initial_import.ms NOT IN (SELECT ms FROM initial);" >> /tmp/sql.cmds #
+echo "UPDATE initial SET initial_decision = (SELECT initial_import.initial_decision FROM initial_import WHERE initial.ms=initial_import.ms), initial_decision_dt = (SELECT initial_import.initial_decision_dt FROM initial_import WHERE initial.ms=initial_import.ms);" >> /tmp/sql.cmds #
 else :
 fi
 
@@ -57,9 +58,10 @@ if [ -e $fullinput ]; then
 egrep "^\"[[:digit:]]*\"," $fullinput > full_tmp.csv
 tr -d "\"" < full_tmp.csv > full.csv
 rm full_tmp.csv
-echo "delete FROM full_import;" >> /tmp/sql.cmds #
+echo "DELETE FROM full_import;" >> /tmp/sql.cmds #
 echo ".import full.csv full_import" >> /tmp/sql.cmds #
-echo "insert INTO full SELECT * FROM full_import WHERE full_import.ms NOT IN (SELECT ms FROM full);" >> /tmp/sql.cmds #
+echo "INSERT INTO full SELECT * FROM full_import WHERE full_import.ms NOT IN (SELECT ms FROM full);" >> /tmp/sql.cmds #
+echo "UPDATE full SET full_decision = (SELECT full_import.full_decision FROM full_import WHERE full.ms=full_import.ms), full_decision_dt = (SELECT full_import.full_decision_dt FROM full_import WHERE full.ms=full_import.ms);" >> /tmp/sql.cmds #
 else :
 fi
 
@@ -69,9 +71,10 @@ if [ -e $rev1input ]; then
 egrep "^\"[[:digit:]]*\"," $rev1input > rev1_tmp.csv
 tr -d "\"" < rev1_tmp.csv > rev1.csv
 rm rev1_tmp.csv
-echo "delete FROM rev1_import;" >> /tmp/sql.cmds #
+echo "DELETE FROM rev1_import;" >> /tmp/sql.cmds #
 echo ".import rev1.csv rev1_import" >> /tmp/sql.cmds #
 echo "insert INTO rev1 SELECT * FROM rev1_import WHERE rev1_import.ms NOT IN (SELECT ms FROM rev1);" >> /tmp/sql.cmds #
+echo "UPDATE rev1 SET rev1_decision = (SELECT rev1_import.rev1_decision FROM rev1_import WHERE rev1.ms=rev1_import.ms), rev1_decision_dt = (SELECT rev1_import.rev1_decision_dt FROM rev1_import WHERE rev1.ms=rev1_import.ms);" >> /tmp/sql.cmds #
 else :
 fi
 
@@ -81,9 +84,10 @@ if [ -e $rev2input ]; then
 egrep "^\"[[:digit:]]*\"," $rev2input > rev2_tmp.csv
 tr -d "\"" < rev2_tmp.csv > rev2.csv
 rm rev2_tmp.csv
-echo "delete FROM rev2_import;" >> /tmp/sql.cmds #
+echo "DELETE FROM rev2_import;" >> /tmp/sql.cmds #
 echo ".import rev2.csv rev2_import" >> /tmp/sql.cmds #
 echo "insert INTO rev2 SELECT * FROM rev2_import WHERE rev2_import.ms NOT IN (SELECT ms FROM rev2);" >> /tmp/sql.cmds #
+echo "UPDATE rev2 SET rev2_decision = (SELECT rev2_import.rev2_decision FROM rev2_import WHERE rev2.ms=rev2_import.ms), rev2_decision_dt = (SELECT rev2_import.rev2_decision_dt FROM rev2_import WHERE rev2.ms=rev2_import.ms);" >> /tmp/sql.cmds #
 else :
 fi
 
@@ -93,9 +97,10 @@ if [ -e $rev3input ]; then
 egrep "^\"[[:digit:]]*\"," $rev3input > rev3_tmp.csv
 tr -d "\"" < rev3_tmp.csv > rev3.csv
 rm rev3_tmp.csv
-echo "delete FROM rev3_import;" >> /tmp/sql.cmds #
+echo "DELETE FROM rev3_import;" >> /tmp/sql.cmds #
 echo ".import rev3.csv rev3_import" >> /tmp/sql.cmds #
 echo "insert INTO rev3 SELECT * FROM rev3_import WHERE rev3_import.ms NOT IN (SELECT ms FROM rev3);" >> /tmp/sql.cmds #
+echo "UPDATE rev3 SET rev3_decision = (SELECT rev3_import.rev3_decision FROM rev3_import WHERE rev3.ms=rev3_import.ms), rev3_decision_dt = (SELECT rev3_import.rev3_decision_dt FROM rev3_import WHERE rev3.ms=rev3_import.ms);" >> /tmp/sql.cmds #
 else :
 fi
 
@@ -105,9 +110,10 @@ if [ -e $rev4input ]; then
 egrep "^\"[[:digit:]]*\"," $rev4input > rev4_tmp.csv
 tr -d "\"" < rev4_tmp.csv > rev4.csv
 rm rev4_tmp.csv
-echo "delete FROM rev4_import;" >> /tmp/sql.cmds #
+echo "DELETE FROM rev4_import;" >> /tmp/sql.cmds #
 echo ".import rev4.csv rev4_import" >> /tmp/sql.cmds #
-echo "insert INTO rev4 SELECT * FROM rev4_import WHERE rev4_import.ms NOT IN (SELECT ms FROM rev4);" >> /tmp/sql.cmds #
+echo "INSERT INTO rev4 SELECT * FROM rev4_import WHERE rev4_import.ms NOT IN (SELECT ms FROM rev4);" >> /tmp/sql.cmds #
+echo "UPDATE rev4 SET rev4_decision = (SELECT rev4_import.rev4_decision FROM rev4_import WHERE rev4.ms=rev4_import.ms), rev4_decision_dt = (SELECT rev4_import.rev4_decision_dt FROM rev4_import WHERE rev4.ms=rev4_import.ms);" >> /tmp/sql.cmds #
 else :
 fi
 
@@ -117,7 +123,7 @@ if [ -e $typeinput ]; then
 egrep "^\"[[:digit:]]*\"," $typeinput > type_tmp.csv
 tr -d "\"" < type_tmp.csv > type.csv
 rm type_tmp.csv
-echo "delete FROM type_import;" >> /tmp/sql.cmds #
+echo "DELETE FROM type_import;" >> /tmp/sql.cmds #
 echo ".import type.csv type_import" >> /tmp/sql.cmds #
 echo "insert INTO type SELECT * FROM type_import WHERE type_import.ms NOT IN (SELECT ms FROM type);" >> /tmp/sql.cmds #
 else :
@@ -126,9 +132,10 @@ fi
 #Publication date data
 
 if [ -e published.csv ]; then
-echo "delete FROM published_import;" >> /tmp/sql.cmds #
+echo "DELETE FROM published_import;" >> /tmp/sql.cmds #
 echo ".import published.csv published_import" >> /tmp/sql.cmds #
-echo "insert INTO published SELECT * FROM published_import WHERE published_import.ms NOT IN (SELECT ms FROM published);" >> /tmp/sql.cmds #
+echo "INSERT INTO published SELECT * FROM published_import WHERE published_import.ms NOT IN (SELECT ms FROM published);" >> /tmp/sql.cmds #
+echo "UPDATE published SET vor_dt = (SELECT published_import.vor_dt FROM published_import WHERE published.ms=published_import.ms);" >> /tmp/sql.cmds #
 else :
 fi
 
@@ -149,13 +156,3 @@ cat /tmp/sql.cmds | sqlite3 elife_paper_stats.sqlite
 # Remove SQL command file
 
 rm /tmp/sql.cmds
-
-# Remove temp .csv files
-
-rm initial.csv
-rm full.csv
-rm rev1.csv
-rm rev2.csv
-rm rev3.csv
-rm rev4.csv
-rm type.csv
